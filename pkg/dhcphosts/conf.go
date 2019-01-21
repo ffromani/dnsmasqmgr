89fdfe41d958aed99d1893ba484890990d7b2703
Francesco Romani <fromani@gmail.com>
2019-01-21 20:01:47 +0100

    pkg: dhcphosts: add package
    the `dhcphosts` package provides utilites to work with
    files in dhcphosts (see man 8 dnsmasq) format.
    
    Signed-off-by: Francesco Romani <fromani@gmail.com>

---
+++ pkg/dhcphosts/conf.go
@@ -0,0 +1,216 @@
+/*
+ * Copyright 2019 Francesco Romani - fromani/gmail
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// the package dhcphosts provides utilities to work with configuration in the dhcphosts
+// (see man 8 dnsmasq) format
+package dhcphosts
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"sync"
+)
+
+var (
+	HWAddrNotFound   error = errors.New("hardware address not found")
+	IPAddrNotFound   error = errors.New("IP address not found")
+	BadHWAddrFormat  error = errors.New("Malformed hardware address address")
+	BadIPFormat      error = errors.New("Malformed IP address")
+	BadBindingFormat error = errors.New("Malformed Binding Pair")
+)
+
+// Binding represents the binding between a MAC and an IP
+type Binding struct {
+	HW net.HardwareAddr
+	IP net.IP
+}
+
+// ParseBindingString parses a string in the dhcphosts format (man 8 dnsmasq) and returns a Binding
+func ParseBindingString(s string) (Binding, error) {
+	a := strings.Split(s, ",")
+	if len(a) != 2 {
+		return Binding{}, BadBindingFormat
+	}
+	return ParseBinding(a[0], a[1])
+}
+
+// ParseBinding creates a Binding between a MAC and a IP, expressed as strings
+func ParseBinding(hw, ip string) (Binding, error) {
+	hwAddr, err := net.ParseMAC(hw)
+	if err != nil {
+		return Binding{}, BadHWAddrFormat
+	}
+	ipAddr := net.ParseIP(ip)
+	if ipAddr == nil {
+		return Binding{}, BadIPFormat
+	}
+	return Binding{
+		HW: hwAddr,
+		IP: ipAddr,
+	}, nil
+}
+
+// EqualHW returns true if the Binding has the MAC part equal to the argument, false otherwise
+func (b Binding) EqualHW(x net.HardwareAddr) bool {
+	return bytes.Equal(b.HW, x)
+}
+
+// EqualIP returns true if the Binding has the IP part equal to the argument, false otherwise
+func (b Binding) EqualIP(x net.IP) bool {
+	return b.IP.Equal(x)
+}
+
+// Equal returns true if the Binding is equal to the argument, false otherwise
+func (b Binding) Equal(bb Binding) bool {
+	return b.EqualHW(bb.HW) && b.EqualIP(bb.IP)
+}
+
+// String converts the binding in its dhcphosts (man 8 dnsmasq) representation
+func (b Binding) String() string {
+	return fmt.Sprintf("%s,%s", b.HW.String(), b.IP.String())
+}
+
+// Conf represents the configured Bindings
+type Conf struct {
+	lock sync.RWMutex
+	// linear search isn't O(1), but it is more than enough for the kind of load we expect
+	bindings []Binding
+	// to support faster delete. Not realley needed, but fun to have
+}
+
+// Len returns the number of configured Bindings
+func (m *Conf) Len() int {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	return len(m.bindings)
+}
+
+// String converts all the registered bindings in the Conf in content in dhcphosts (man 8 dnsmasq) representation
+func (m *Conf) String() string {
+	var sb strings.Builder
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	for _, bi := range m.bindings {
+		sb.WriteString(fmt.Sprintf("%s\n", bi.String()))
+	}
+	return sb.String()
+}
+
+// Add registers a new Binding
+func (m *Conf) Add(b Binding) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	return m.add(b)
+}
+
+func (m *Conf) add(b Binding) error {
+	// TODO: check dupes?
+	m.bindings = append(m.bindings, b)
+	return nil
+}
+
+// Add forgets a Binding previously Add()ed
+func (m *Conf) Delete(b Binding) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	var bindings []Binding
+	for _, bi := range m.bindings {
+		if bi.Equal(b) {
+			continue
+		}
+		bindings = append(bindings, bi)
+	}
+	m.bindings = bindings
+	return nil
+}
+
+func find(data findable, x interface{}, err error) (interface{}, error) {
+	for i := 0; i < data.Len(); i++ {
+		bi := data.Get(i)
+		if data.Equal(bi, x) {
+			return bi, nil
+		}
+	}
+	return data.Empty(), err
+}
+
+// inspired by the sort package
+type findable interface {
+	Len() int
+	Get(i int) interface{}
+	Equal(a, b interface{}) bool
+	Empty() interface{}
+}
+
+type byHW []Binding
+
+func (a byHW) Len() int                    { return len(a) }
+func (a byHW) Get(i int) interface{}       { return a[i] }
+func (a byHW) Empty() interface{}          { return Binding{} }
+func (a byHW) Equal(x, y interface{}) bool { return x.(Binding).EqualHW(y.(Binding).HW) }
+
+type byIP []Binding
+
+func (a byIP) Len() int                    { return len(a) }
+func (a byIP) Get(i int) interface{}       { return a[i] }
+func (a byIP) Empty() interface{}          { return Binding{} }
+func (a byIP) Equal(x, y interface{}) bool { return x.(Binding).EqualIP(y.(Binding).IP) }
+
+func (m *Conf) GetByHWAddr(hw string) (Binding, error) {
+	x, err := net.ParseMAC(hw)
+	if err != nil {
+		return Binding{}, err
+	}
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	ret, err := find(byHW(m.bindings), Binding{HW: x}, HWAddrNotFound)
+	return ret.(Binding), err
+}
+
+func (m *Conf) GetByIP(ip string) (Binding, error) {
+	x := net.ParseIP(ip)
+	if x == nil {
+		return Binding{}, BadIPFormat
+	}
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+	ret, err := find(byIP(m.bindings), Binding{IP: x}, IPAddrNotFound)
+	return ret.(Binding), err
+}
+
+// Parse creates a Conf from a reader, which must return content in dhcphosts (man 8 dnsmasq) format
+func Parse(r io.Reader) (*Conf, error) {
+	m := &Conf{}
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		b, err := ParseBindingString(s.Text())
+		if err != nil {
+			return nil, err
+		}
+		m.add(b)
+	}
+	return m, nil
+}
---
+++ pkg/dhcphosts/conf_test.go
@@ -0,0 +1,234 @@
+/*
+ * Copyright 2019 Francesco Romani - fromani/gmail
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies
+ * or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package dhcphosts
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"testing"
+)
+
+func TestBindingParseError(t *testing.T) {
+	var err error
+
+	_, err = ParseBindingString("")
+	if err != BadBindingFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	_, err = ParseBindingString("01:23:45:67:89:ab")
+	if err != BadBindingFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	_, err = ParseBindingString("1.1.1.1")
+	if err != BadBindingFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// legal in dnsmasq.conf, but unsupported yet
+	_, err = ParseBindingString("01:23:45:67:89:ab,1.1.1.1,extra")
+	if err != BadBindingFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	_, err = ParseBindingString("malformed_mac,1.1.1.1")
+	if err != BadHWAddrFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	_, err = ParseBindingString("01:23:45:67:89:ab,malformed_IP")
+	if err != BadIPFormat {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+func TestBindingParseRoundTrip(t *testing.T) {
+	s := "01:23:45:67:89:ab,1.1.1.1"
+	b, err := ParseBindingString(s)
+	if err != nil {
+		t.Errorf("parsing b1: unexpected error: %v", err)
+	}
+	x := b.String()
+	if s != x {
+		t.Errorf("failed roundtrip: %s %s", s, x)
+	}
+}
+
+func TestBindingEqual(t *testing.T) {
+	b1, err := ParseBinding("01:23:45:67:89:ab", "1.1.1.1")
+	if err != nil {
+		t.Errorf("parsing b1: unexpected error: %v", err)
+	}
+	b2, err := ParseBinding("01:23:45:67:89:ab", "2.2.2.2")
+	if err != nil {
+		t.Errorf("parsing b2: unexpected error: %v", err)
+	}
+	b3, err := ParseBinding("fe:dc:ba:98:76:54", "2.2.2.2")
+	if err != nil {
+		t.Errorf("parsinbg b3: unexpected error: %v", err)
+	}
+	if b1.Equal(b2) || b1.Equal(b3) {
+		t.Errorf("unexpectedly equal: %v %v %v", b1, b2, b3)
+	}
+	if !b1.EqualHW(b2.HW) {
+		t.Errorf("unexpectedly different HW: %v %v", b1, b2)
+	}
+	if !b2.EqualIP(b3.IP) {
+		t.Errorf("unexpectedly different IP: %v %v", b1, b2)
+	}
+}
+
+func TestConfEmpty(t *testing.T) {
+	c := &Conf{}
+	L := c.Len()
+	if L != 0 {
+		t.Errorf("length %v not zero", L)
+	}
+	var err error
+	_, err = c.GetByIP("1.1.1.1")
+	if err != IPAddrNotFound {
+		t.Errorf("unexpected error: %v", err)
+	}
+	_, err = c.GetByHWAddr("01:23:45:67:89:ab")
+	if err != HWAddrNotFound {
+		t.Errorf("unexpected error: %v", err)
+	}
+}
+
+const testData string = "" +
+	"01:23:45:67:89:ab,1.1.1.1\n" +
+	"fe:dc:ba:98:76:54,2.2.2.2\n" +
+	""
+
+func TestConfLoadFile(t *testing.T) {
+	tmpfile, err := mktmpfile(testData)
+	if err != nil {
+		t.Errorf("unexpected error creating tmpfile: %v", err)
+	}
+	defer cleanup(tmpfile)
+
+	m, err := Parse(tmpfile)
+	if err != nil {
+		t.Errorf("unexpected error reading tmpfile: %v", err)
+	}
+	L := m.Len()
+	if L != 2 {
+		t.Errorf("unexpected number of entries: %v", L)
+	}
+}
+
+func TestFindWithContent(t *testing.T) {
+	ref, err := ParseBindingString("fe:dc:ba:98:76:54,1.1.1.1")
+	if err != nil {
+		t.Errorf("unexpected error parsing the ref data: %v", err)
+	}
+
+	sr := strings.NewReader(testData)
+	m, err := Parse(sr)
+	if err != nil {
+		t.Errorf("unexpected error reading tmpfile: %v", err)
+	}
+	L := m.Len()
+	if L != 2 {
+		t.Errorf("unexpected number of entries: %v", L)
+	}
+
+	b1, err := m.GetByHWAddr("01:23:45:67:89:ab")
+	if err != nil {
+		t.Errorf("unexpected error looking by HWAddr: %v", err)
+	}
+	if !b1.EqualIP(ref.IP) {
+		t.Errorf("ref mismatch: [%s] [%s]", b1.IP.String(), ref.IP.String())
+	}
+
+	b2, err := m.GetByIP("2.2.2.2")
+	if err != nil {
+		t.Errorf("unexpected error looking by I{: %v", err)
+	}
+	if !b2.EqualHW(ref.HW) {
+		t.Errorf("ref mismatch: [%s] [%s]", b2.IP.String(), ref.IP.String())
+	}
+}
+
+func TestConfDumpFile(t *testing.T) {
+	sr := strings.NewReader(testData)
+	m, err := Parse(sr)
+	if err != nil {
+		t.Errorf("unexpected error reading tmpfile: %v", err)
+	}
+	L := m.Len()
+	if L != 2 {
+		t.Errorf("unexpected number of entries: %v", L)
+	}
+
+	tmpfile, err := mktmpfile("")
+	if err != nil {
+		cleanup(tmpfile)
+		t.Errorf("unexpected error creating tmpfile: %v", err)
+	}
+	_, err = tmpfile.WriteString(m.String())
+	if err != nil {
+		cleanup(tmpfile)
+		t.Errorf("unexpected error dumping to tmpfile: %v", err)
+	}
+	defer os.Remove(tmpfile.Name())
+	err = tmpfile.Close()
+	if err != nil {
+		t.Errorf("unexpected error closing tmpfile: %v", err)
+	}
+
+	content, err := ioutil.ReadFile(tmpfile.Name())
+	if err != nil {
+		t.Errorf("unexpected error reading back tmpfile: %v", err)
+	}
+	if string(content) != testData {
+		t.Errorf("inconsistent content: %v\n%v\n", string(content), testData)
+	}
+}
+
+func cleanup(tmpfile *os.File) {
+	tmpfile.Close()
+	os.Remove(tmpfile.Name())
+}
+
+func mktmpfile(content string) (*os.File, error) {
+	tmpfile, err := ioutil.TempFile("", "dhcphosts")
+	if err != nil {
+		return nil, err
+	}
+	if _, err := tmpfile.WriteString(content); err != nil {
+		tmpfile.Close()
+		return nil, err
+	}
+	if err := tmpfile.Sync(); err != nil {
+		tmpfile.Close()
+		return nil, err
+	}
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		tmpfile.Close()
+		return nil, err
+	}
+	return tmpfile, nil
+}
