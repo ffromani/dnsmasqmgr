31546b15af545853481c2ab850f3f2cf617f6ca4
Francesco Romani <fromani@gmail.com>
2019-03-31 19:31:49 +0200

    dnsmasqmgr: implement real client
    Signed-off-by: Francesco Romani <fromani@gmail.com>

--- cmd/dnsmasqmgr/main.go
+++ cmd/dnsmasqmgr/main.go
@@ -24,38 +24,138 @@ package main
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
+	flag "github.com/spf13/pflag"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/mojaves/dnsmasqmgr/pkg/dnsmasqmgr"
 )
 
-const (
-	address = "localhost:50777"
+var (
+	certFile = flag.String("certfile", "", "The TLS cert file")
+	keyFile  = flag.String("keyfile", "", "The TLS key file")
+	timeout  = flag.Int("timeout", 1, "The connection timeout")
+	iface    = flag.String("interface", "127.0.0.1", "The server listening interface")
+	port     = flag.Int("port", 50777, "The server port")
 )
 
+func addrToJson(a *pb.Address) string {
+	return fmt.Sprintf("{ \"name\": \"%s\", \"mac\": \"%s\", \"ip\": \"%s\" }", a.Hostname, a.Macaddr, a.Ipaddr)
+}
+
+type Queryable interface {
+	Name() string
+	SetupArgs(args []string) error
+	RunWith(ctx context.Context, c pb.DNSMasqManagerClient) (string, error)
+}
+
+type QueryLookup struct {
+	name string
+	key  pb.Key
+	addr *pb.Address
+}
+
+func (ql *QueryLookup) Name() string {
+	return ql.name
+}
+
+func (ql *QueryLookup) SetupArgs(args []string) error {
+	// args:
+	// - lookup
+	// - how
+	// - what
+	ql.name = args[0]
+	if len(args) < 3 {
+		return fmt.Errorf("not enough arguments: `%v`", args[1:])
+	}
+	if len(args) > 3 {
+		return fmt.Errorf("too many arguments: `%v`", args[1:])
+	}
+	switch args[1] {
+	case "name":
+		ql.key = pb.Key_HOSTNAME
+		ql.addr = &pb.Address{
+			Hostname: args[2],
+		}
+	default:
+		return fmt.Errorf("%s: unsupported method: %s", args[0], args[1])
+	}
+	return nil
+}
+
+func (ql *QueryLookup) RunWith(ctx context.Context, c pb.DNSMasqManagerClient) (string, error) {
+	r, err := c.LookupAddress(ctx, &pb.AddressRequest{
+		Addr: ql.addr,
+		Key:  ql.key,
+	})
+	return addrToJson(r.Addr), err
+}
+
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage %s [options] subcommand args:\n", filepath.Base(os.Args[0]))
+		fmt.Fprintf(os.Stderr, "subcommands:\n")
+		fmt.Fprintf(os.Stderr, "- lookup how what:\n")
+		fmt.Fprintf(os.Stderr, "  * how:  one of 'name', 'mac', 'ip'\n")
+		fmt.Fprintf(os.Stderr, "  * what: entity to lookup\n")
+		fmt.Fprintf(os.Stderr, "options:\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	var query Queryable
+	switch args[0] {
+	case "lookup":
+		query = &QueryLookup{}
+	default:
+		fmt.Fprintf(os.Stderr, "Unsupported subcommand %s\n", args[0])
+		os.Exit(1)
+	}
+
+	err := query.SetupArgs(args)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		fmt.Fprintf(os.Stderr, "Cannot setup %s: %v\n", query.Name(), err)
+		os.Exit(2)
+	}
+
+	var opts []grpc.DialOption
+	if *certFile != "" && *keyFile != "" {
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to generate credentials %v\n", err)
+			os.Exit(8)
+		}
+		opts = []grpc.DialOption{grpc.WithTransportCredentials(creds)}
+	} else {
+		opts = []grpc.DialOption{grpc.WithInsecure()}
+	}
+
+	address := fmt.Sprintf("%s:%d", *iface, *port)
+	conn, err := grpc.Dial(address, opts...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not connect: %v\n", err)
+		os.Exit(16)
 	}
 	defer conn.Close()
 	c := pb.NewDNSMasqManagerClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(*timeout)*time.Second)
 	defer cancel()
 
-	r, err := c.LookupAddress(ctx, &pb.AddressRequest{
-		Addr: &pb.Address{
-			Hostname: "test",
-		},
-		Key: pb.Key_HOSTNAME,
-	})
+	out, err := query.RunWith(ctx, c)
 	if err != nil {
-		log.Fatalf("could not lookup: %v", err)
+		fmt.Fprintf(os.Stderr, "Error performing: %s: %v\n", query.Name(), err)
 	}
-	log.Printf("%s: mac=%s ip=%s", r.Addr.Hostname, r.Addr.Macaddr, r.Addr.Ipaddr)
+	fmt.Printf("%s\n", out)
 }
